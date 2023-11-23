13ee7e730e42d4dee5c699db28f0694bc2c2a784
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-23 19:51:06 +0300

    111

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Login_page():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def authorization(self, user_field, password_field):
+        user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='user-name']")))
+        user_name.send_keys(user_field)
+        print("Login entered")
+
+        password = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))
+        password.send_keys(password_field)
+        print("Password entered")
+        time.sleep(3)
+
+        button_login = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
+        button_login.click()
+        print("Login clicked")
