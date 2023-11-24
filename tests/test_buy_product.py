9894d84885ed0f35f4189dd5380d4ae87123756d
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-24 23:21:33 +0300

    111

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -3,10 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.login_page import Login_page
 
-def test_select_product(self):
+def test_select_product():
     driver = webdriver.Chrome()
     print("Test started")
+
+    login = Login_page(driver)
+    login.authorization()
+
 
     open_cart = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='shopping_cart_container']")))
     open_cart.click()
