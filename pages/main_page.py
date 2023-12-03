1eaddc8282792dddf76580b9f81161d2108c3afc
okarpuk <oleg.karpuk.1982@gmail.com>
2023-12-03 22:40:28 +0300

    222

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class Login_page(Base):
+class Main_page(Base):
 
     def __init__(self, driver):
         super().__init__(driver)
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import Login_page
+from pages.main_page import Main_page
+
 
 def test_buy_product():
     driver = webdriver.Chrome()
@@ -12,7 +14,7 @@ def test_buy_product():
     login = Login_page(driver)
     login.authorization()
 
-    open_cart = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='shopping_cart_container']")))
-    open_cart.click()
-    print("Cart opened")
+    mp = Main_page(driver)
+    mp.select_product()
+
     time.sleep(3)
