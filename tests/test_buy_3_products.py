0d31a2b900e03ae5534eed3f4a891163521c13d4
okarpuk <oleg.karpuk.1982@gmail.com>
2023-12-07 21:20:06 +0300

    222

--- tests/test_buy_3_products.py
+++ tests/test_buy_3_products.py
@@ -1,12 +1,11 @@
 import time
+import pytest
 from selenium import webdriver
 from pages.cart_page import Cart_page
-from pages.last_page import Last_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
-from pages.payment_page import Payment_page
-from pages.user_info_page import User_info_page
 
+@pytest.mark.order(3)
 def test_buy_product_1():
     driver = webdriver.Chrome()
     print("\nTest 1 started")
@@ -20,19 +19,11 @@ def test_buy_product_1():
     cp = Cart_page(driver)
     cp.confirm_product()
 
-    # uip = User_info_page(driver)
-    # uip.confirm_user_info()
-    #
-    # pp = Payment_page(driver)
-    # pp.click_finish_button()
-    #
-    # lp = Last_page(driver)
-    # lp.create_screenshot()
-
     print("Test 1 finished")
     time.sleep(3)
     driver.quit()
 
+@pytest.mark.order(1)
 def test_buy_product_2():
     driver = webdriver.Chrome()
     print("\nTest 2 started")
@@ -50,6 +41,7 @@ def test_buy_product_2():
     time.sleep(3)
     driver.quit()
 
+@pytest.mark.order(2)
 def test_buy_product_3():
     driver = webdriver.Chrome()
     print("\nTest 3 started")
